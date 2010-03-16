27ab74ac80f394d94de1ad8671759b6c41a28102
Devgru <java@devg.ru>
2010-03-17 01:21:28 +0300

    + some categories mappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -2,8 +2,11 @@ class UrlMappings {
     static mappings = {
 
         "/$page"(action: 'index', controller: 'main')
-        "/articles/*"(action: 'index', controller: 'main')
-        "/articles/*/*"(action: 'index', controller: 'main')
+        "/articles/**"(action: 'index', controller: 'main')
+        "/blog/**"(action: 'index', controller: 'main')
+        "/religion/**"(action: 'index', controller: 'main')
+        "/psychology/**"(action: 'index', controller: 'main')
+        "/groovy/**"(action: 'index', controller: 'main')
 
         "/$controller/$id?"(action: 'index')
 
