ece2d40fad99158dcc6fbe24894cae2c607dd89e
Devgru <java@devg.ru>
2010-03-17 15:06:45 +0300

    - removed unused DataSources

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -9,24 +9,3 @@ hibernate {
     cache.use_query_cache=true
     cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
 }
-// environment specific settings
-environments {
-	development {
-		dataSource {
-			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			url = "jdbc:hsqldb:mem:devDB"
-		}
-	}
-	test {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:hsqldb:mem:testDb"
-		}
-	}
-	production {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-		}
-	}
-}
