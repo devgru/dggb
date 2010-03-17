04a56707b331de839c662059c4156a7b97e38f60
Devgru <java@devg.ru>
2010-03-17 16:04:56 +0300

    ~ trying to play with UrlMappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,14 +1,9 @@
 class UrlMappings {
+     static excludes = ["/images/*", "/css/**", "/WEB-INF/*"]
+
     static mappings = {
 
-        "/$page"(action: 'index', controller: 'main')
-        "/articles/**"(action: 'index', controller: 'main')
-        "/blog/**"(action: 'index', controller: 'main')
-        "/religion/**"(action: 'index', controller: 'main')
-        "/psychology/**"(action: 'index', controller: 'main')
-        "/groovy/**"(action: 'index', controller: 'main')
-
-        "/$controller/$id?"(action: 'index')
+        "/$page**"(action: 'index', controller: 'main')
 
         "/"(view: '/index')
         "500"(view: '/error')
