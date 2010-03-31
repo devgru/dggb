eb72ac1d52c9759d34b9e0aeb6c1e5cb565a124b
Devgru <java@devg.ru>
2010-03-31 18:34:43 +0400

    - index css @ index

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,5 +1,5 @@
 eventCompileStart = {
-    def files = ['main', 'index']
+    def files = ['main']
     println "Running lessc on $files"
     files.each() {
         System.out << "/var/lib/gems/1.9.1/bin/lessc web-app/css/${it}.less".execute().errorStream;
