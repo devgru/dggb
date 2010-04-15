adad0655e9af7ac047927fc4147c33b1c29a5b4f
Devgru <java@devg.ru>
2010-04-15 15:56:37 +0400

    + twitter integration

---
+++ grails-app/services/ru/devg/dggb/TwitterService.groovy
@@ -0,0 +1,21 @@
+package ru.devg.dggb
+
+import twitter4j.Twitter;
+import twitter4j.Status;
+
+class TwitterService {
+
+    boolean transactional = false
+
+    static final Twitter twitter = new Twitter()
+    List<Status> statuses
+
+    void updateStatuses() {
+
+        def statuses = twitter.getUserTimeline('devgru');
+        statuses = statuses.grep {!it.text.startsWith('@')}.subList(0, 5)
+        this.statuses = statuses 
+        log.trace "statuses updated (${statuses.size()})"
+
+    }
+}
