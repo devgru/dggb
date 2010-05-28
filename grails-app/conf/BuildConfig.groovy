8b9b8f298ae213b773ab4b47fae5e7cd5e5e76be
Devgru <java@devg.ru>
2010-05-28 23:00:13 +0400

    ~ playing with config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,14 +1,14 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.war.file = "target/home.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits( "global" ) {
         // uncomment to disable ehcache
         // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "warn"
     repositories {        
         grailsPlugins()
         grailsHome()
