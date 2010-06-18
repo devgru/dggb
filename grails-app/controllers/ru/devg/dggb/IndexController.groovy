44691f3e1fcfdc66ce6fb1c7467f9e270e9d6745
Devgru <java@devg.ru>
2010-06-19 03:24:01 +0400

    + Last-Modified for index controller

--- grails-app/controllers/ru/devg/dggb/IndexController.groovy
+++ grails-app/controllers/ru/devg/dggb/IndexController.groovy
@@ -8,6 +8,7 @@ class IndexController {
     def twitterService
 
     def index = {
+        response.addDateHeader 'Last-Modified', new Date(2010, 01, 01).getTime()
         statuses = twitterService.statuses
         render(view: '/index')
     }
