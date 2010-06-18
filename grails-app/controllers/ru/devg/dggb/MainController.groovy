76ec520f16014250e48a6790273f570d7be7b31d
Devgru <java@devg.ru>
2010-06-19 03:04:09 +0400

    + Last-Modified header @ main controller

--- grails-app/controllers/ru/devg/dggb/MainController.groovy
+++ grails-app/controllers/ru/devg/dggb/MainController.groovy
@@ -13,14 +13,18 @@ class MainController {
         url = urlService.getUrl()
         entry = entryService.entry
 
-        if (entry && entry['custom layout']) {
+        if(entry == null) {
+            render(view: '/error', status: 404)
+            return
+        }
+
+        response.addDateHeader 'Last-Modified' , entry.file.lastModified()
+        if (entry['custom layout']) {
             render(view: 'empty')
         } else if (entry instanceof Directory) {
             render(view: 'directory')
         } else if (entry instanceof Page) {
             render(view: 'page')
-        } else {
-            render(view: '/error', status: 404)
         }
     }
 }
